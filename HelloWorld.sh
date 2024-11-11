da9e35519322014dfb67ee601d3bb5c139c5139c
Vivek Talatam <vivektalatam2000@gmail.com>
2024-11-11 16:51:49 +0530

    Updating changes in .sh file

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -1,2 +1,9 @@
 #!bin/bash
 echo "Yo Hello World, pushing the first .sh file"
+
+name1=Anders
+name2=Zephyr(Gae)
+echo "$name1: Hey There, $name2!"
+echo "$name2: Yo $name1"
+echo "$name1: How u doin?"
+echo "$name2: I'm fine, what about you $name1?"
