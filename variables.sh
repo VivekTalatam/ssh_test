6fe1f9bf2ab5dc18e9a2b40004139a9c2f5fd89d
Vivek Talatam <vivektalatam2000@gmail.com>
2024-11-12 11:41:45 +0530

    Updating changes

--- variables.sh
+++ variables.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 DATE=$(date)
 echo " Hi today's timestamp is: $DATE"
 echo "List'em All $(ls -lart)"
---
+++ variables_2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+Number1=$1
+Number2=$2
+SUM=$((Number1+Number2))
+echo "Value:- $SUM"
