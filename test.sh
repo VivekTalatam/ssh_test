cf87f656171d0ca8e8e55a8eb41e0732ebb40fbb
Vivek Talatam <vivektalatam2000@gmail.com>
2024-11-13 16:22:50 +0530

    Adding test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "Enter Today's Day:- "
 read TODAY
-if (TODAY=="sunday") {
+if [TODAY=="sunday"] 
+then
     echo "Yo, Let's Chill!"
-}
-else { 
+
+else 
     echo "Let's get back to work :("
-}
+fi
