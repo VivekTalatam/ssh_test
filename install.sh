5e88ee476ab264c73c13639d1827c9fcedf4070a
Vivek Talatam <vivektalatam2000@gmail.com>
2024-11-14 12:07:01 +0530

    Adding install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,20 @@ then
     echo "Error:: Please run this script with root access"
     exit 1
 fi
-yum update -y
-yum install https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
-
-if [ $? -ne 0 ]
+VALIDATE(){
+    if [ $? -ne 0 ]
 then
     echo "Installation failed..."
+    exit 1
 else
     echo "Installation successful..."
 fi
+}
+yum update -y
+yum install https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm -y
+VALIDATE $? "Installing mysql..."
+yum install postfix -y
+VALIDATE $? "Installing postfix..."
+
+
  
