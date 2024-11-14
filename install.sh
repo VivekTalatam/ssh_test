021a300580f55b7f4a11f951dde3f5566b3abf2c
Vivek Talatam <vivektalatam2000@gmail.com>
2024-11-14 12:40:13 +0530

    Adding install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-userid=$(id -u)
-if [ $userid -ne 0 ]
-then
-    echo "Error:: Please run this script with root access"
-    exit 1
-fi
+DATE=$(date +%F:%H:%M:%S)
+SCRIPt_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 VALIDATE(){
     if [ $? -ne 0 ]
 then
@@ -14,10 +11,17 @@ else
     echo "$2 successful..."
 fi
 }
-yum update -y
-yum install https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm -y
+userid=$(id -u)
+if [ $userid -ne 0 ]
+then
+    echo "Error:: Please run this script with root access"
+    exit 1
+fi
+
+yum update -y &>>$LOGFILE
+yum install https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm -y &>>$LOGFILE
 VALIDATE $? "Installing mysql"
-yum install postfix -y
+yum install postfix -y &>>$LOGFILE
 VALIDATE $? "Installing postfix"
 
 
