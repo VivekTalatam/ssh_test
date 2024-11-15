2769681b31e0770a58876bfd206c332d54b33d69
Vivek Talatam <vivektalatam2000@gmail.com>
2024-11-15 15:06:29 +0530

    adding loops.sh

--- install_with_loop.sh
+++ install_with_loop.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
+DATE=$(date +%F:%H:%M:%S)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+R=\e[31m
+G=\e[32m
+N=\e[0m
+CHECK(){
+    if [ $? -ne 0 ]
+    then
+        echo -e "$2 $R....failed $N"
+        exit 1
+    else
+        echo -e "$2 $G....Successful $N"
+    fi
+}
+
+user= $(id -u)
+if [ $user -ne 0 ]
+then
+    echo -e " $R ERROR:: Please run the script with sudo permissions $N"
+    exit 1
+fi
 for i in $@
 do 
     yum install $i -y
+    CHECK $? $1 $2
 done
